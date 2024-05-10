93ccdb70e775db2e7b771e6af3519d7b8f079bc6
Emerson2310 <emerson-fx@hotmail.com>
2024-05-09 23:40:59 -0300

    Desafio conta de banco.

---
+++ Declaracao-argumentos/CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        int num;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome!");
+        nomeCliente = teclado.nextLine();
+
+        System.out.println("Digite o número de sua conta!");
+        num = teclado.nextInt();
+
+        System.out.println("Por favor, digite o número da agência!");
+        agencia = teclado.next();
+
+        System.out.println("Digite seu saldo bancário!");
+        saldo = teclado.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso " +
+        "banco, sua agência é " + agencia + ", conta " + num + " e seu saldo " + saldo +" já está disponível para saque");
+
+        teclado.close();
+    }
+}
