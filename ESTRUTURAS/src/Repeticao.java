908cdfac5e0406465ea6983a44a9666b383756ed
Emerson2310 <emerson-fx@hotmail.com>
2024-05-14 17:25:29 -0300

    Criação de uma classe para testar repetição

---
+++ ESTRUTURAS/src/Repeticao.java
@@ -0,0 +1,12 @@
+public class Repeticao {
+    public static void main(String[] args) throws Exception {
+       
+        String alunos [] = {"Alberto","João","Maria","Marina"};
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O Aluno no indíce " + x + " é: " + alunos [x]);
+        }
+
+        
+    }
+}
