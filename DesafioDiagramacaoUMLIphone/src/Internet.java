76817356f29294b1dccc405cd8fa4abecfda87d0
Emerson2310 <emerson-fx@hotmail.com>
2024-07-01 22:44:12 -0300

    Desafio Iphone

---
+++ DesafioDiagramacaoUMLIphone/src/Internet.java
@@ -0,0 +1,7 @@
+public interface Internet {
+
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+ 
+}
---
+++ DesafioDiagramacaoUMLIphone/src/Iphone.java
@@ -0,0 +1,38 @@
+public class Iphone implements Ipod, Phone, Internet{
+
+    @Override
+    public void tocar() {
+    }
+
+    @Override
+    public void pausar() {
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+    }
+    
+    @Override
+    public void ligar(String numero) {
+    }
+
+    @Override
+    public void atender() {
+    }
+
+    @Override
+    public void iniciarCorreioVoz() { 
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+    }
+
+    @Override
+    public void atualizarPagina() {
+    }
+}
