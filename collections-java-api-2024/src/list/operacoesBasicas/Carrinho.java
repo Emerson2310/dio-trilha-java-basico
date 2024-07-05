5a30c61ccd1c7867e66e5ab678bcea4d91cd7fb9
Emerson2310 <emerson-fx@hotmail.com>
2024-07-04 23:51:39 -0300

    atualizando

--- collections-java-api-2024/src/list/operacoesBasicas/Carrinho.java
+++ collections-java-api-2024/src/list/operacoesBasicas/Carrinho.java
@@ -1,4 +1,23 @@
 package list.operacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrinho {
+    private List<Item> itens;
+    public Carrinho() {
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem (String nome, double preco, int qtde){
+        itens.add(new Item(nome, preco, qtde));
+    }
+
+    public void removerItem (String nome){
+        for(Item i: itens){
+            if(nome.equalsIgnoreCase(i.getNome())){
+                itens.remove(i);
+            }
+        }
+    }
 }
